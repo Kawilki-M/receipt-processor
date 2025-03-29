2bbb0074f0e20d3bf0b12217cc2fea159f8b3f47
Kayden Wilkinson <69224099+Kawilki-M@users.noreply.github.com>
2025-03-29 14:18:36 -0700

    Allow configuration of server port through env variable and add server bubble text

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,22 +1,38 @@
-package main 
+package main
 
 import (
 	"fmt"
 	"net/http"
+	"os"
 
-	"github.com/go-chi/chi"
 	"github.com/Kawilki-M/receipt-processor/internal/handlers"
+	"github.com/go-chi/chi"
 	log "github.com/sirupsen/logrus"
 )
 
-func main(){
+func main() {
+	fmt.Println("Starting Receipt Processing Service...")
+
 	log.SetReportCaller(true)
 	var r *chi.Mux = chi.NewRouter()
 	handlers.Handler(r)
 
-	fmt.Println("Starting Receipt Processing Service...")
+	fmt.Println(`
+   __               _       _       ___                                        
+  /__\ ___  ___ ___(_)_ __ | |_    / _ \_ __ ___   ___ ___  ___ ___  ___  _ __ 
+ / \/// _ \/ __/ _ \ | '_ \| __|  / /_)/ '__/ _ \ / __/ _ \/ __/ __|/ _ \| '__|
+/ _  \  __/ (_|  __/ | |_) | |_  / ___/| | | (_) | (_|  __/\__ \__ \ (_) | |   
+\/ \_/\___|\___\___|_| .__/ \__| \/    |_|  \___/ \___\___||___/___/\___/|_|   
+                     |_|                                                       `)
 
-	err := http.ListenAndServe("localhost:8000", r)
+	port, exists := os.LookupEnv("PORT") // Default to 8000 if PORT is not set
+	if !exists {
+		port = "8000"
+	}
+
+	fmt.Printf("\nServer running on localhost:%s", port)
+
+	err := http.ListenAndServe("localhost:"+port, r)
 	if err != nil {
 		log.Error(err)
 	}
